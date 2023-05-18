b5f9c7f7396c7d4d7a60c823c43209360575e4d0
saurabh singh <86177535+saurabhsngh5484@users.noreply.github.com>
2023-05-18 10:03:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+const table = document.querySelector("table");
+const lastRow = document.createElement("tr");
+// const prices = document.querySelectorAll("[data-ns-test=price]");
+let allRows = document.getElementsByTagName("tr");
+let sum = 0;
+for (let i = 0; i < allRows.length; i++) {
+  let tds = allRows[i].getElementsByTagName("td");
+  sum += parseInt(tds[2].textContent);
+}
+
+// for (let i = 0; i < prices.length; i++) {
+//   sum += parseInt(prices[i].textContent);
+// }
+const child = document.createElement("td");
+child.setAttribute("data-ns-test", "grandTotal");
+
+child.textContent = sum;
+lastRow.appendChild(child);
+table.appendChild(lastRow);
